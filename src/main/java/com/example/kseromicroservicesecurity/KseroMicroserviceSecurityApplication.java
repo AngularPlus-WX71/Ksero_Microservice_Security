723890287ceda5a0d8996ae912c62c50205f1eaa
Draacckeer <u201922975@upc.edu.pe>
2022-09-29 20:12:43 -0500

    Initial files

---
+++ src/main/java/com/example/kseromicroservicesecurity/KseroMicroserviceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.kseromicroservicesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KseroMicroserviceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KseroMicroserviceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kseromicroservicesecurity/KseroMicroserviceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kseromicroservicesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KseroMicroserviceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
